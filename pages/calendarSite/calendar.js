7c94d7c3d22e49006042caf1c6ced82669c8b45e
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-04-20 08:37:14 +0200

    forfra kalender js

---
+++ pages/calendarSite/calendar.js
@@ -0,0 +1,21 @@
+//Funktion - Ændring af farver på klik af datoer/ugedagene?
+
+// Variabel 
+
+// Array - [....?]
+
+// Loop - 
+
+// Objekt 
+
+
+const ugedage = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
+
+var farver = ["black", "white"];
+
+function changeColor(element, color) {
+    element.style.Color = color;
+}
+
+// <onMouseDown="changeColor(this, farver[0])" onMouseUp="this, farver[1]">
+
