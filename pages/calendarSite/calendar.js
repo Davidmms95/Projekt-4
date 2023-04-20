ebd3568b89eef6e50a4754127eaa2a08c88d3b61
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-04-20 10:12:21 +0200

    Update calendar.js

--- pages/calendarSite/calendar.js
+++ pages/calendarSite/calendar.js
@@ -1,21 +1,2 @@
-//Funktion 
-
-// Variabel 
-
-// Array 
-
-// Loop 
-
-// Objekt 
-
-
-const ugedage = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
-
-var farver = ["black", "white"];
-
-function changeColor(element, color) {
-    element.style.Color = color;
-}
-
-// <onMouseDown="changeColor(this, farver[0])" onMouseUp="this, farver[1]">
+indsæt funktion fra hjem
 
