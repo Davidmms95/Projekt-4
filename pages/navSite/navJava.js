9941d71bc36ef464040160b57270e545a3c6f0d4
slavehanni <Johannes.hanni@hotmail.com>
2023-04-21 13:33:00 +0200

    knap

--- pages/navSite/navJava.js
+++ pages/navSite/navJava.js
@@ -13,3 +13,23 @@ function hideMessage() {
   document.getElementById("popup-message").style.display = "none";
   document.getElementById("afl-kasse").style.backgroundColor = "green";
 }
+
+
+let seMereClicked = false;
+
+function toggleContent() {
+  const additionalContent = document.getElementById("additional");
+  const seMereButton = document.getElementById("se-mere");
+
+  if (seMereClicked) {
+    additionalContent.classList.add("hidden");
+    seMereButton.textContent = "Se kommende afleveringer";
+  } else {
+    additionalContent.classList.remove("hidden");
+    seMereButton.textContent = "Se nuværende afleveringer";
+  }
+
+  seMereClicked = !seMereClicked;
+}
+
+document.getElementById("se-mere").addEventListener("click", toggleContent)
